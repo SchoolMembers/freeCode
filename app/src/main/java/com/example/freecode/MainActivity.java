e4eb076053cea74241855e3e0bbdbf9099eefd45
Yujini03 <youjin6812@gmail.com>
2025-01-04 19:59:35 +0900

    [add] 메인 화면 메뉴 열고 닫기 기능

--- app/src/main/java/com/example/freecode/MainActivity.java
+++ app/src/main/java/com/example/freecode/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.freecode;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.freecode.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //메뉴 바 열고 닫기
+        Button menuOpenButton = binding.openNavigation;
+        Button menuCloseButton = binding.closeNavigation;
+        ScrollView navView = binding.navView;
+        NavigationBarView navRail = binding.navigation; //네비게이션 레일
+
+        menuOpenButton.setOnClickListener(v -> {
+            navView.setVisibility(View.VISIBLE);
+            menuOpenButton.setVisibility(View.GONE);
+            menuCloseButton.setVisibility(View.VISIBLE);
+        });
+
+        menuCloseButton.setOnClickListener(v -> {
+            navView.setVisibility(View.GONE);
+            menuOpenButton.setVisibility(View.VISIBLE);
+            menuCloseButton.setVisibility(View.GONE);
         });
     }
 }
